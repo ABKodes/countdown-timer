63787283e4e4d6cafb0e474e92e66e5af08f0917
ABKodes <abiyshiferaw12@gmail.com>
2024-02-23 23:21:37 +0300

    configuration

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -4,7 +4,7 @@ function Countdown() {
   <div className="bg-blue-500 h-20 w-20 relative">
     <div className="absolute left-0 top-1/2 w-full h-0.5 bg-white"></div>
   </div>
-  <div className="absolute top-0 left-0 bg-red-500 h-20 w-20"></div> 
+  <div className="absolute top-0 left-0 bg-red-500 h-20 w-20">hey</div> 
 </div>
 
 
