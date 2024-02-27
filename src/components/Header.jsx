218ac593136465b993b2a9342ea8224c652e48ef
ABKodes <abiyshiferaw12@gmail.com>
2024-02-27 15:47:10 +0300

    setting up the work environment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
 function Header() {
   return (
-    <h1 className="m-auto p-4 text-center font-red-hat text-5xl font-bold uppercase tracking-widest text-white w-10/12">
-      We're launching soon
-    </h1>
+    <div>
+      <h1 className="text-center font-red-hat text-3xl md:text-5xl font-bold uppercase tracking-widest text-white">
+        Countdown Timer
+      </h1>
+      <p className="font-red-hat text-center md:text-xl text-white">
+        Stay tuned as we count down to exciting events, product launches, or
+        special offers.
+      </p>
+    </div>
   );
 }
 
