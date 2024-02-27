8b51c27ad5886b27ce71ca067e51fc08f56ab39b
ABKodes <abiyshiferaw12@gmail.com>
2024-02-27 15:46:30 +0300

    setting up the work environment

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,13 +1,54 @@
 function Countdown() {
   return (
-    <div className="relative">
-  <div className="bg-blue-500 h-20 w-20 relative">
-    <div className="absolute left-0 top-1/2 w-full h-0.5 bg-white"></div>
-  </div>
-  <div className="absolute top-0 left-0 bg-red-500 h-20 w-20">hey</div> 
-</div>
-
-
+    <>
+      <div className="flex justify-center space-x-3">
+        <div className="items-center space-y-2">
+          <div className="flex h-16 w-16 items-center justify-center bg-[#343650] md:h-20 md:w-20">
+            <p className="font-red-hat text-4xl text-[#fb6087] md:text-8xl">
+              3
+            </p>
+          </div>
+          <p className="text-center font-red-hat text-xs uppercase tracking-widest text-white md:text-sm">
+            Days
+          </p>
+        </div>
+        <div className="items-center space-y-2">
+          <div className="flex h-16 w-16 items-center justify-center bg-[#343650] md:h-20 md:w-20">
+            <p className="font-red-hat text-4xl text-[#fb6087] md:text-8xl">
+              3
+            </p>
+          </div>
+          <p className=" text-center font-red-hat text-xs uppercase  tracking-widest text-white md:text-sm">
+            Hours
+          </p>
+        </div>
+        <div className="items-center space-y-2">
+          <div className="flex h-16 w-16 items-center justify-center bg-[#343650] md:h-20 md:w-20">
+            <p className="font-red-hat text-4xl text-[#fb6087] md:text-8xl">
+              3
+            </p>
+          </div>
+          <p className="text-center font-red-hat text-xs uppercase tracking-widest text-white md:text-sm">
+            Minutes
+          </p>
+        </div>
+        <div className="items-center space-y-2">
+          <div className="flex h-16 w-16 items-center justify-center bg-[#343650] md:h-20 md:w-20">
+            <p className="font-red-hat text-4xl text-[#fb6087] md:text-8xl">
+              3
+            </p>
+          </div>
+          <p className="text-center font-red-hat text-xs uppercase tracking-widest text-white md:text-sm">
+            Seconds
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-center my-3">
+        <button className="rounded-md bg-[#343650] p-2 font-red-hat text-white ring ring-[#fb6087] hover:bg-opacity-60 hover:text-[#fb6087]">
+          Start
+        </button>
+      </div>
+    </>
   );
 }
 
